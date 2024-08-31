65b4734336eb6d42b47aaac5e64504359f3b3ca1
Kevinniv <92663312+Kevinniv@users.noreply.github.com>
2024-08-31 20:06:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -190,4 +190,5 @@ def extract_watermark():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    # app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    pass
