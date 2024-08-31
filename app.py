cbbd97bb0f41983879891fb2d1f966865d0676b6
Kevinniv <92663312+Kevinniv@users.noreply.github.com>
2024-08-31 20:29:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -152,5 +152,5 @@ def extract_watermark():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 
